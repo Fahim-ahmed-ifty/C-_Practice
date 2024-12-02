a0587a7eb0cc605bae36834a7d583dc4b5cf33e7
fahim-ahmed-ifty <fahimahmediftyzettabyte@gmail.com>
2024-12-02 11:36:57 +0600

    ✅

---
+++ Practice_2024/12/2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    int a = 2;
+    int b = 4;
+    int c = a + b;
+    int d = b - a;
+    int e = b / a;
+    int f = b * a;
+
+    for (int i = 0; i < 10;i++){
+        std::cout << "*\n";
+    }
+
+        // std::cout << "Hello Fahim and " << c << ' ' << d << ' ' << e << ' ' << f << std::endl;
+    return 0;
+}
